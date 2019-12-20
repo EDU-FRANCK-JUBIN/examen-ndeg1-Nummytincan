381cab1abb234097814d8a32830ce03cb6515c4c
Nummytincan <44395281+Nummytincan@users.noreply.github.com>
2019-12-20 11:26:49 +0100

    ajout turtle

---
+++ turtle.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.color("green")
+turtle.pendown()
+turtle.forward(100)
+
+#je n'arrive pas à utiliser turtle, comme si je n'avais pas la librairie mais
+#elle est bien installé sur mon environnement anaconda
+# de plus il me dit que turtle n'as pas de method color x)
